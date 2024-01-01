bb0b3f511f25edaf269d3cd39e742662dba4b488
Uday <budayteja009@gmail.com>
2024-01-01 15:05:37 +0530

    Intial Setup

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,9 +1,9 @@
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect, isSelected }) {
   // console.log(props);
   console.log('TAB COMPONENT EXECUTING...');
   return (
     <li>
-      <button onClick={onSelect}>{children}</button>
+      <button className={isSelected ? "active" : undefined} onClick={onSelect}>{children}</button>
     </li>
-  )
+  );
 }
