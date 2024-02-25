e0bb9eede685a257c7445273144bafee1e0a5422
Uday <budayteja009@gmail.com>
2024-02-26 03:20:45 +0530

    Add unit testing for contact component

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -2,22 +2,35 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom"
 
-test("should load contact us component", () => {
-  render(<Contact />);
 
-  const heading = screen.getByRole("heading");
+describe("Contact Component", () => {
+  test("should load contact us component", () => {
+    render(<Contact />);
 
-  // Assertion
-  expect(heading).toBeInTheDocument();
+    const heading = screen.getByRole("heading");
+
+    // Assertion
+    expect(heading).toBeInTheDocument();
+  });
+
+  test("should load inside contact component", () => {
+    render(<Contact />);
+
+    // const button = screen.getByRole("button");
+    //      (or)
+    const button = screen.getByText("Submit");
+
+    // Assertion
+    expect(button).toBeInTheDocument();
+  });
+
+  it("Should load 2 input boxes on the Contact component", () => {
+    render(<Contact />);
+
+    const inputBoxes = screen.getAllByRole("textbox");
+
+    // Assertion
+    expect(inputBoxes.length).toBe(2);
+  });
 });
 
-test("should load inside contact component", () => {
-  render(<Contact />);
-
-  // const button = screen.getByRole("button");
-
-  const button = screen.getByText("Submit");
-
-  // Assertion
-  expect(button).toBeInTheDocument();
-});
